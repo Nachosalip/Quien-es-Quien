dac11f87b5bdc15bc95d717ed8f83bff011984eb
NachoSalip <47128485@est.ort.edu.ar>
2023-08-11 12:00:11 -0300

    tengo que seguir code

--- Assets/Scripts/ControlDialogos.cs
+++ Assets/Scripts/ControlDialogos.cs
@@ -6,4 +6,33 @@ public class controlDialogos : MonoBehaviour
 {
    private animator anim;
    private Queue <string> filaDialogos;
+   Textos texto;
+}
+
+public void ActivarCartel(Textos textoObjeto)
+{
+   anim.setBool("Cartel", true)
+   texto = textoObjeto;
+
+}
+
+public void ActivaTexto()
+{
+   filaDialogos.clear();
+   foreach (string textoGuardar in texto.arrayTextos)
+   {
+      filaDialogos.Enqueue(textoGuardar)
+   }
+   siguienteFrase();
+}
+
+public void siguienteFrase()
+{
+   CierraCartel();
+   return;
+}
+
+void CierraCartel()
+{
+   anim.setBool("Cartel", false);
 }
