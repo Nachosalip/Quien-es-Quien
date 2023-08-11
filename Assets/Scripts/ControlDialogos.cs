2381f76e9dd4fe5fb5a5a7348dd97e7b999487aa
NachoSalip <47128485@est.ort.edu.ar>
2023-08-11 11:37:38 -0300

    Seguimos pantalla de juego/Scripts no terminados

---
+++ Assets/Scripts/ControlDialogos.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlDialogos : MonoBehaviour
+{
+   private animator anim;
+   private Queue <string> filaDialogos;
+}
---
+++ Assets/Scripts/Textos.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Textos
+{
+    [TextArea (2,6)]
+    public string[] arrayTextos
+}
