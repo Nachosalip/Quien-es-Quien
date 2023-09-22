f2914b86e86de0e8df98959535b6e35502751b67
NachoSalip <47128485@est.ort.edu.ar>
2023-09-22 09:36:27 -0300

    Dejamos de usar la BDD

---
+++ Assets/Scripts/TamaraLaprida.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje2D
+{
+    public string Nombre { get; set; }
+    public string Accesorios { get; set; }
+    public string Ojos { get; set; }
+    public string Pelo { get; set; }
+    public string Genero { get; set; }
+}
+
+Public DatosPersonajes(string nombre, string accesorios, string ojos, string pelo, string genero)
+{
+    Nombre = "Tamara Laprida";
+    Accesorios = "No tiene";
+    ojos = "Negros";
+    pelo = "Negro";
+    genero = "Femenino";
+}
